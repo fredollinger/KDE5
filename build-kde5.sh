cb370d9f54e8745ed686f839f41b74fd1d770ca1
Fred Ollinger <follinge@gmail.com>
2012-02-18 10:57:10 -0800

    first commit

---
+++ build-kde5.sh
@@ -0,0 +1,6 @@
+sudo apt-get install x11proto-dri2-dev x11proto-gl-dev   libxdamage-dev x11proto-damage-dev libexpat1-dev libxt-dev libxmuu-dev  libxi-dev libxmu-dev mesa-common-dev libpcre3-dev libgl1-mesa-dev
+
+sudo apt-get build-dep kdelibs5-dev/natty
+
+#apt-get source mesa
+
