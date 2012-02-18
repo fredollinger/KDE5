79a40f450650760a71dff396881b1c836cebc9de
Fred Ollinger <follinge@gmail.com>
2012-02-18 14:19:32 -0800

    Added version 5 of modules.

--- build-kde5.sh
+++ build-kde5.sh
@@ -1,6 +1,7 @@
-sudo apt-get install x11proto-dri2-dev x11proto-gl-dev   libxdamage-dev x11proto-damage-dev libexpat1-dev libxt-dev libxmuu-dev  libxi-dev libxmu-dev mesa-common-dev libpcre3-dev libgl1-mesa-dev
+sudo apt-get install x11proto-dri2-dev x11proto-gl-dev   libxdamage-dev x11proto-damage-dev libexpat1-dev libxt-dev libxmuu-dev  libxi-dev libxmu-dev mesa-common-dev libpcre3-dev libgl1-mesa-devlibcups2-dev
 
 sudo apt-get build-dep kdelibs5-dev/natty
+sudo apt-get build-dep libqt4-dev/natty
 
 #apt-get source mesa
 
