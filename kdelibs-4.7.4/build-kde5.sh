1b5eba0398e27f01ce184f6430c6908d91f4a05e
Fred Ollinger <follinge@gmail.com>
2012-02-18 16:53:43 -0800

    Added some more needed files. Still won't build b/c of nonsense libsm fail.

--- kdelibs-4.7.4/build-kde5.sh
+++ kdelibs-4.7.4/build-kde5.sh
@@ -1,4 +1,9 @@
-export PATH=../../kde5/qt5/bin
-qmake -v
-#mkdir -p build
-#cd build && cmake ..
+#!/bin/sh
+
+export QTDIR=../../qt5
+export PATH=$QTDIR/qtbase/bin:$PATH
+export LD_LIBRARY_PATH=$QTDIR/lib:../../../kde5/lib:$LD_LIBRARY_PATH:/host/kd5/lib
+export PKG_CONFIG_PATH=../../../kde5/lib/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig
+
+mkdir -p build
+cd build && cmake ..
